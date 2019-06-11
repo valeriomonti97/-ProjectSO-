c685ac9a5d1429972f6318a5593f8bbe14cdb32b
valeriomonti97 <valeriomonti97@yahoo.it>
2019-06-11 18:30:48 +0200

    Add semwait

--- disastrOS_semwait.c
+++ disastrOS_semwait.c
@@ -8,4 +8,31 @@
 
 void internal_semWait(){
   // do stuff :)
+
+  /*For get the sem_d we need the fd from the PCB of the running process */
+  int fd = running -> syscall_args[0];
+  SemDescriptor* semdesc = SemDescriptorList_byFd(&running->sem_descriptors, fd);
+  if (!semdesc){
+    running -> syscall_retvalue = DSOS_ESEMWAIT_SEMD_NOT_IN_PROCESS;
+    return;
+  }
+
+  /*Decrease the count of s. If the count is negative insert the running proc in wait list */
+  Semaphore* s = semdesc->semaphore;
+  assert(s);
+  s->count--;
+  PCB* pcb = running;
+  if (s->count < 0){
+    SemDescriptorPtr* semdesc_ptr = SemDescriptorPtr_alloc(semdesc);
+    asser(semdesc_ptr);
+    List_insert(&s->waiting_descriptors, s->waiting_descriptors.last, (ListItem*)semdesc_ptr);
+    running->status = Waiting;
+    List_insert(&waiting_list, waiting_list.last, (ListItem*) running);
+    running = (PCB*)List_detach(&ready_list, ready_list.first);
+    running -> status = Running;
+  }
+
+  /*Set the ret value of syscall */
+  pcb->syscall_retvalue = 0;
+
 }
