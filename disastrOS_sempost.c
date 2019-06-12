dd807e7b16dd4376950b2351f64f497600cfc0cf
valeriomonti97 <valeriomonti97@yahoo.it>
2019-06-12 11:32:27 +0200

    Add sempost

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -8,4 +8,26 @@
 
 void internal_semPost(){
   // do stuff :)
+
+  int fd = running -> syscall_args[0];
+  SemDescriptor semdesc = SemDescriptorList_byFd(&running -> sem_descriptors, fd);
+  if (!semdesc){
+    running -> syscall_retvalue = DSOS_ESEMPOST_SEMD_NOT_IN_PROCESS;
+    return;
+  }
+
+  Semaphore* s = semdesc -> semaphore;
+  assert(sem);
+  sem->count++;
+  if (sem->count <= 0){
+    SemDescriptorPtr* semdesc_ptr = (SemDescriptorPtr*)List_detach(&s->waiting_descriptors, s->waiting_descriptors.first);
+    PCB* pcb = semdesc_ptr->descriptor->pcb;
+    SemDescriptorPtr_free(semdesc_ptr);
+    pcb->status = Ready;
+    List_detach(&waiting_list, (ListItem*)pcb);
+    List_insert(&ready_list, ready_list.last, (ListItem*)pcb);
+  }
+
+  running->syscall_retvalue = 0;
+
 }
